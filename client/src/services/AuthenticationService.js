b752b348ee1a3d83d7089c1d4f1eb18b5809b903
john beren <jmberean@gmail.com>
2018-11-14 11:16:21 -0500

    yes

--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import Api from '@/services/Api'
-=======
-// Your error is here. Not in your component. you see how you are missing a slash after the @?
-import Api from '@services/Api'
->>>>>>> c77266063b55ede5acb643177ca61fafd701b059
+
 // export Object that has register 
 export default{
     register (credentials) {
