708c3ef8f7667eb45724754d36435549d35fcc76
john beren <jmberean@gmail.com>
2018-11-14 00:37:05 -0500

    trying to get auth to work

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -20,7 +20,7 @@
 </template>
 
 <script>
-import AuthenticationService from '@/services/AuthenticationService'
+//import AuthenticationService from '@//services/AuthenticationService'
 export default {
   data () {
     return {
