0ef29a9f43217f371a65a18a7425b6f32ca233f9
john beren <jmberean@gmail.com>
2018-11-14 13:05:50 -0500

    DB implemenetation

--- server/src/app.js
+++ server/src/app.js
@@ -1,25 +1,22 @@
-console.log(20)
 const express = require('express');
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
 const app = express();
+
 app.use(morgan('combined'))
-app.use(express.json());
+app.use(bodyParser.json())
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/status', (req,res)=> {
-    res.send({
-        message: 'helloworld'
-    })
-})
+require('./routes')(app)
 
-app.post('/register', (req,res)=> {
-    res.send({
-        message: `Hello ${req.body.email}! User registered!`
-
-    })
+sequelize.sync()
+    .then(()=>{
+        app.listen(config.port)
+        console.log(`Server started on port ${config.port}`)
 })
 
 app.listen(process.env.PORT || 8081)
-// APP ID: 432637970601508
