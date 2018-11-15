9cbe6e79418af161a35a782fbc3199219fea367e
john beren <jmberean@gmail.com>
2018-11-15 16:19:30 -0500

    fixed db

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,19 +1,19 @@
-// const Sequelize = require('sequelize')
-// const Op = Sequelize.Op
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 module.exports={
-    port:8081,
-    // port:process.env.PORT||8081,
+    //port:8081,
+    port:process.env.PORT||8082,
 
     db:{
     database: process.env.DB_NAME || 'app1',
     user:process.env.DB_USER || 'app1',
     password:process.env.DB_PASS || 'app1',
-    options:{
-        dialect:process.env.DIALECT || 'sqlite',
-        host:process.env.HOST || 'localhost',
-        storage:'./app1.sqlite',
-        //operatorsAliases: Op
-    }
+        options:{
+            dialect:process.env.DIALECT || 'sqlite',
+            host:process.env.HOST || 'localhost',
+            storage:'./app1.sqlite',
+            operatorsAliases: Op
+        }
     }
 }
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,9 +1,8 @@
-module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
+module.exports = (sequelize, DataTypes) =>
+    sequelize.define('User', {
       email: {
         type: DataTypes.STRING,
         unique: true
       },
       password: DataTypes.STRING
-    }
-    })
+})
